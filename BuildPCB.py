a8c740914bc451c6be50153702653db71620a12f
Matt Jenkins <matt@majenko.co.uk>
2019-01-22 00:31:49 +0000

    Rebuild of script and splitting of models into smaller blend files

--- BuildPCB.py
+++ BuildPCB.py
@@ -1,32 +1,29 @@
 import bpy
 import csv
 import sys
-import re
+import os
 
-file_root       = "/home/matt/Dropbox/Projects/AutoPowerSwitch/Gerber"
-file_name       = "AutoPowerSwitch"
+file_root       = "/home/matt/Dropbox/Projects/Marek/Mainboard/Gerber/"
+file_name       = "Mainboard"
+offset_x = 10
+offset_y = 40
+file_outline    = file_root + file_name + ".outline.svg"
+file_drill      = file_root + file_name + ".plated-drill.cnc"
+file_csv        = file_root + file_name + ".xy"
 file_components = "/home/matt/Dropbox/gEDA/Models/components.blend"
-
-file_outline    = file_root + "/" + file_name + ".outline.svg"
-file_outlinegbr = file_root + "/" + file_name + ".outline.gbr"
-file_drill      = file_root + "/" + file_name + ".plated-drill.cnc"
-file_csv        = file_root + "/" + file_name + ".xy"
-
-m = [re.findall(r'\*X(\d+)Y(\d+)D', line)
-    for line in open(file_outlinegbr)]
-
-offset_x = 0
-offset_y = 0
-
-for i in m:
-    if (len(i) > 0):
-        offset_x = int(i[0][0]) / 1000.0
-        offset_y = int(i[0][1]) / 1000.0
-
+component_root = "/home/matt/Dropbox/gEDA/Models/components"
 
 rotations = {
-
+    "U7" : 360,
+    "CONN1" : 90,
+    "CONN2" : 270,
+    "CONN5" : 270
 }
+
+txt = bpy.data.texts.get("BuildReport.txt")
+if not txt:
+    txt = bpy.data.texts.new("BuildReport.txt")
+txt.clear()
 
 
 def subtract(target, opObj):
@@ -40,9 +37,6 @@ def subtract(target, opObj):
    bpy.context.scene.update()
    target.data = target.to_mesh(bpy.context.scene, True, 'PREVIEW')
    target.modifiers.remove(mod)
-   #bpy.ops.object.modifier_apply(apply_as='DATA', modifier=mod[0].name)
-   
-
 
 gotPCB = False
 try:
@@ -59,16 +53,14 @@ if not gotPCB:
     bpy.ops.object.select_all(action='DESELECT')
     for c in newcurves:
         c.select = True
-        
     bpy.context.scene.objects.active = newcurves[0]
     bpy.ops.object.join()
     curve = bpy.context.object
-    curve.scale = [28.888, 28.888, 1]
+    curve.scale = [36, 36, 1]
     bpy.ops.object.convert(target='MESH')
     bpy.ops.object.mode_set(mode='EDIT', toggle=False)
     bpy.ops.mesh.select_all(action='SELECT')
-    bpy.ops.mesh.remove_doubles()
-
+    bpy.ops.mesh.remove_doubles(threshold=0.01)
     bpy.ops.mesh.extrude_edges_move(
         MESH_OT_extrude_edges_indiv={
             "mirror": False
@@ -95,25 +87,17 @@ if not gotPCB:
     bpy.ops.mesh.select_all(action='SELECT')
     bpy.ops.mesh.edge_face_add()
     bpy.ops.mesh.select_all(action='SELECT')
-    #bpy.ops.uv.smart_project()
     bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
-
     pcb = bpy.context.object
     pcb.name = 'PCB'
     pcb.location = [offset_x, offset_y, -1.6]
-
     with open(file_drill, newline='', encoding='ISO-8859-15') as f:
         content = f.read().splitlines()
-        
     drills = {}
     inHeader = True
     drillWidth = 0
-
-
     bits = []
-
     nbits = 0
-
     for line in content:
         if line == '%':
             inHeader = False
@@ -125,7 +109,6 @@ if not gotPCB:
         else:
             if line.startswith('T'):
                 drillWidth = drills[line]
-                #bit.dimensions=[drillWidth, drillWidth, 50]
                 continue
             if line.startswith('X'):
                 nbits = nbits + 1
@@ -138,30 +121,19 @@ if not gotPCB:
                 newbit = bpy.context.object
                 newbit.name="Drill Bit." + str(nbits)
                 newbit.dimensions=[drillWidth,drillWidth,50]
-                #newbit = bit.copy()
-                #bpy.context.scene.objects.link(newbit)
                 bits.append(newbit)
                 newbit.location=[x, y, 0]
-                #subtract(pcb, bit)
-
-
     bpy.ops.wm.redraw_timer(type='DRAW_WIN_SWAP', iterations=1)
-
     bpy.ops.object.select_all(action='DESELECT')
     for abit in bits:
         abit.select = True
-
     bpy.context.scene.objects.active = bits[0]
     bpy.ops.object.join()
-
     allbits = bpy.context.object
-
     subtract(pcb, allbits)
-
     bpy.ops.object.select_all(action='DESELECT')
     allbits.select = True
     bpy.ops.object.delete()
-
     bpy.ops.object.select_all(action='DESELECT')
     bpy.context.scene.objects.active = pcb
     bpy.ops.object.mode_set(mode='EDIT', toggle=False)
@@ -175,55 +147,38 @@ with open(file_csv, newline='', encoding='ISO-8859-15') as fobj:
     reader = csv.reader(filter(lambda row: row[0] != '#', fobj))
     layout_table = list(reader)
 
-required = set(col[1] for col in layout_table)
+required = list(col[1] for col in layout_table)
 
-with bpy.data.libraries.load(file_components, link=True) as (data_from, data_to):
-    available = set(data_from.meshes)
-    missing = required - available
-    data_to.meshes = list(required & available)
+compfiles = []
+for compfile in os.listdir(component_root):
+    if compfile.lower().endswith('.blend'):
+        compfiles.append(component_root + os.sep + compfile)
 
-if missing:
-    txt = bpy.data.texts.get("missing_components.txt")
-    if not txt:
-        txt = bpy.data.texts.new("missing_components.txt")
-    txt.clear()
-    misscount = 0
-    for comp in missing:
-        if comp == 'DNP':
-            continue
-        if comp == '(unknown)':
-            continue
-        txt.write(comp + "\n")
-        misscount = misscount + 1
-    if misscount == 0:
-        txt.write("There are no missing components.\n")
-else:
-    txt = bpy.data.texts.get("missing_components.txt")
-    if not txt:
-        txt = bpy.data.texts.new("missing_components.txt")
-    txt.clear()
-    txt.write("There are no missing components\n")
-
+for compfile in compfiles:
+    txt.write("Loading models from " + compfile + "\n")
+    with bpy.data.libraries.load(compfile, link=True) as (data_from, data_to):
+        found = [value for value in data_from.meshes if value in required]
+        for f in found:
+            txt.write("    Found: " + f + "\n")
+        data_to.meshes = found
+        required = [value for value in required if value not in data_from.meshes]
+        
+txt.write("\nMissing components:\n")
+for missing in required:
+    txt.write("    " + missing + "\n")
+        
 objects_data  = bpy.data.objects
 objects_scene = bpy.context.scene.objects
-name_to_index = {mesh.name: index for index, mesh in enumerate(data_to.meshes)}
-
-def create_dupli(id, name, location, rotation):
-    oname = id + " - " + name
-    for ob in bpy.data.objects:
-        if ob.name.startswith(id + " - "):
-            ob.select = True
-            bpy.ops.object.delete()
-    try:
-        mesh = data_to.meshes[name_to_index[name]]
-    except KeyError:
-        return
-    dupli = objects_data.new(oname, mesh)
-    dupli.location = location
-    dupli.rotation_euler = rotation
-    objects_scene.link(dupli)
 
 bpy.ops.object.select_all(action='DESELECT')
+
+print("START")
+
+for lib in bpy.data.libraries:
+    lib.reload()
+    
+for mesh in bpy.data.meshes:
+    print(mesh)
 
 for id, name, value, x, y, rot, side in layout_table:
     z = 0
@@ -240,6 +195,16 @@ for id, name, value, x, y, rot, side in layout_table:
         pass
     frot = frot / 57.2957795
     zrot = tuple(float(val) for val in (0, yrot, frot))
-    create_dupli(id, name, loc, zrot)
 
+    oname = id + " - " + name
+    for ob in bpy.data.objects:
+        if ob.name.startswith(id + " - "):
+            ob.select = True
+            bpy.ops.object.delete()
+            
+    mesh = bpy.data.meshes.get(name)
+    dupli = objects_data.new(oname, mesh)
+    dupli.location = loc
+    dupli.rotation_euler = zrot
+    objects_scene.link(dupli)
 
